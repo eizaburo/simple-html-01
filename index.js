9efa2d842e67441254f1fbc54ee7f86a7e5b804e
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2023-12-30 07:39:08 +0900

    JSスクリプトを完成

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const _name_error = document.getElementById("name_error");
 const _email_error = document.getElementById("email_error");
 const _body_error = document.getElementById("body_error");
 
+const email_exp = /^[a-z0-9.]+@[a-z0-9.]+\.[a-z]{2,4}$/;
+const body_exp = /^.{1,10}$/;
+
 btn_submit.addEventListener("click", (e) => {
     e.preventDefault();
 
@@ -15,11 +18,11 @@ btn_submit.addEventListener("click", (e) => {
         _name_error.classList.remove("hidden");
     }
 
-    if (_email.value == "") {
+    if (!email_exp.test(_email.value)) {
         _email_error.classList.remove("hidden");
     }
 
-    if (_body.value == "") {
+    if (!body_exp.test(_body.value)) {
         _body_error.classList.remove("hidden");
     }
 
@@ -27,5 +30,29 @@ btn_submit.addEventListener("click", (e) => {
         _email_error.classList.contains("hidden") &&
         _body_error.classList.contains("hidden")) {
         alert(`name=${_name.value}, email=${_email.value}, body=${_body.value}`);
+    }
+});
+
+_name.addEventListener("keyup", (e) => {
+    if (_name.value == "") {
+        _name_error.classList.remove("hidden");
+    } else {
+        _name_error.classList.add("hidden");
+    }
+});
+
+_email.addEventListener("keyup", (e) => {
+    if (!email_exp.test(_email.value)) {
+        _email_error.classList.remove("hidden");
+    } else {
+        _email_error.classList.add("hidden");
+    }
+});
+
+_body.addEventListener("keyup", (e) => {
+    if (!body_exp.test(_body.value)) {
+        _body_error.classList.remove("hidden");
+    } else {
+        _body_error.classList.add("hidden");
     }
 });
